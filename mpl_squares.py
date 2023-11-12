7bdd9fb994499b50f03b847715c7a44e08cd3e74
yjb23tech <yjb23tech@gmail.com>
2023-11-12 22:04:17 +0000

    In a hurry

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,13 @@
-import matplotlib.pylot as plt 
+import matplotlib.pyplot as plt 
 
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
 ax.plot(squares)
 
-plt.show()
+#Passing the argument block=False in order to allow me to close the matplotlib window from the terminal by simply pressing 'Enter'
+plt.show(block=False)
+#Subsequent code below again contributes to my ability to close the window simply by pressing 'Enter' in the terminal
+plt.pause(1)
+input()
+plt.close()

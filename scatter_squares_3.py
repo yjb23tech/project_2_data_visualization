c38eb1fcbc3067da402f48977c05e1a5dea51250
yjb23tech <yjb23tech@gmail.com>
2023-11-12 23:26:37 +0000

    Ragnar Lothbrook

--- scatter_squares_3.py
+++ scatter_squares_3.py
@@ -5,11 +5,13 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, s=10)
 
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Values", fontsize=14)
+
+ax.axis([0, 1100, 0, 1100000])
 
 ax.tick_params(axis='both', which='major', labelsize=14)
 

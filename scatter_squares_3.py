5169b244872a12587f94b8c85bdfb4842af0844b
yjb23tech <yjb23tech@gmail.com>
2023-11-12 23:23:11 +0000

    Burn it all

---
+++ scatter_squares_3.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=200)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Values", fontsize=14)
+
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show(block=False)
+plt.pause(1)
+input()
+plt.close()
+
